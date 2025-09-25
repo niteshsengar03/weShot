cb466c92d26da3d012d1400b050809ed8d298521
Nitesh <niteshsengar9831@gmail.com>
2025-09-26 05:01:58 +0530

    add

--- backend/src/routers/v1/student.router.ts
+++ backend/src/routers/v1/student.router.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createStudentController } from '../../controllers/student.controllers';
 import { validateBody } from '../../validator';
 import { studentSchema } from '../../validator/student.validator';
+import { importStudent } from '../../controllers/import.controllers';
 const studentRouter = express.Router();
 
 studentRouter.post('/create-student',validateBody(studentSchema),createStudentController);
